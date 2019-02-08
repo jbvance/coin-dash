771c10093542b22d099d4b9ef0c2ac4649851a5b
Jason Vance <=>
2019-02-08 11:12:38 -0600

    add TopSection, CoinGrid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -7,6 +7,7 @@ import CoinGrid from './CoinGrid';
 export default () => (
     <Page name="settings">
         <WelcomeMessage />
+        <CoinGrid topSection />
         <ConfirmButton />
         <CoinGrid />
     </Page>
