7cb2b2d4d35b176808027ab246aba2e12616f92c
Jason Vance <=>
2019-02-08 10:44:45 -0600

    Add CoinTile

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { fontSize1, greenBoxShadow, color3 } from '../Shared/Styles';
 
 const ConfirmButtonStyled = styled.div`
     margin: 20px;
-    color: green;
+    color: ${color3}
+    padding: 10px;
+    ${fontSize1}
     cursor: pointer;
+    &:hover {
+        ${greenBoxShadow}
+    }
 `;
 
 export const CenterDiv = styled.div`
