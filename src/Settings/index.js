35b980192295e61abdb9ac6263407f3a7e4564f4
Jason Vance <=>
2019-02-08 09:59:25 -0600

    add Styles.js

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default () => (
     <Page name="settings">
         <WelcomeMessage />
         <ConfirmButton />
+        <CoinGrid />
     </Page>
 );
